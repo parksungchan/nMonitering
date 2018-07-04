0b2dc7ed390ce8e03e05585ee9ba2ccce978966d
parksungchan <soul8085@gmail.com>
2018-07-05 07:09:04 +0900

    update

--- report.py
+++ report.py
@@ -1,1 +1,17 @@
 # mariadb
+import mysql.connector as mariadb
+
+mariadb_connection = mariadb.connect(user='hee', password='', database='flybeach')
+cursor = mariadb_connection.cursor()
+
+
+
+
+
+
+
+
+
+
+
+
