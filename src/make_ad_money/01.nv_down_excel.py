06a5a319eba8a302026d2a31e837e97a1e421e1e
parksungchan <tjdcks80@poscoict.com>
2023-02-05 08:45:04 +0900

    update

--- src/make_ad_money/01.nv_down_excel.py
+++ src/make_ad_money/01.nv_down_excel.py
@@ -18,6 +18,8 @@ from selenium.webdriver.common.keys import Keys
     드라이버를 다운받는다. /nMonitering/master/chromedriver에 넣어준다.
     http://chromedriver.storage.googleapis.com/index.html
     
+    config.json에서 nv_ad_info, nv_ad_url 가 작성되어 있어야 한다.
+    
 '''
 # 기존 다운 받으려는 키워드 파일 삭제
 rootdir = 'C:Users'
@@ -31,7 +33,8 @@ for rootdir, dirs, files in os.walk(rootdir):
                 os.remove(file_path)
         break
 print('[Complete] Download Folder keyword list delete.')
-# money 디렉토리 삭제
+
+# 키워드 분석한 결과 money 디렉토리 삭제
 for dr in os.listdir(config.dirs.data_dir):
     if dr.find('money_') > -1:
         shutil.rmtree(os.path.join(config.dirs.data_dir, dr))
@@ -61,7 +64,7 @@ driver.find_element(By.XPATH, '//*[@id="container"]/div/div/fieldset/div/div/but
 # 광고 시스템 클릭
 driver.find_element(By.XPATH, '//*[@id="container"]/my-screen/div/div[1]/div/my-screen-board/div/div[1]/ul/li[1]/a').click()
         
-# 상세 광고로 이동 하여 excel download
+# 상세 광고로 이동 하여 pc, mobile excel 을 구분 해서 저장해 주어야 한다
 for pc_mb in config.nv_ad_url.__dict__:
     print('[Connect Start] ' + pc_mb)
     for link in config.nv_ad_url.__dict__[pc_mb].__dict__:
@@ -69,21 +72,24 @@ for pc_mb in config.nv_ad_url.__dict__:
         if lk == '':
             continue
         print(link, lk)
+
+        # download page로 이동
         driver.get(lk)
         time.sleep(2)
+
+        # excel download button 클릭
         driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[1]/div[4]/div/div[1]/div/div/div[1]/div[1]/div[2]/div/button').click()
         time.sleep(2)
 
-        # move file
-        # pc 와 모바일을 구분해서 저장해주어야 한다. download에 있는 파일을 data 폴더의 money로 옮겨준다.
+        # download에 있는 파일을 data 폴더의 money로 옮겨준다.
         money_dir = os.path.join(config.dirs.data_dir, 'money_' + pc_mb)
         os.makedirs(money_dir, exist_ok=True)
-
         move_list = [x for x in os.listdir(download_dir) if x.find('키워드') > -1]
         for move in move_list:
             src_path = os.path.join(download_dir, move)
             trg_path = os.path.join(money_dir, link + '.xlsx')
             shutil.move(src_path, trg_path)
+
     print('[Connect Complete] ' + pc_mb)
     print('')
     time.sleep(2)
