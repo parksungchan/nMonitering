e295b269150288e33af8592b08ae8e18f0919f8c
parksungchan <tjdcks80@poscoict.com>
2023-02-11 18:22:44 +0900

    update

--- src/02.nv_pw_seq.py
+++ src/02.nv_pw_seq.py
@@ -51,7 +51,6 @@ def nv_down_seq():
                     }
                 }
     '''
-    pg_list = [1, 2]  # 총 2page 의 50건에 대해서만 체크 한다.
     url_json = {'pc': 'https://ad.search.naver.com/search.naver?where=ad&query='
                 , 'mb': 'https://m.ad.search.naver.com/search.naver?where=m_expd&query='}
 
@@ -60,6 +59,9 @@ def nv_down_seq():
     for mp in key_json:
         key_pd = []
         ct_json = key_json[mp]
+        pg_list = [1, 2]  # 총 2page 의 50건에 대해서만 체크 한다.
+        if mp == 'mb':
+            pg_list = [1]
         for ct in ct_json:
             k_json = ct_json[ct]
             for key in k_json:
@@ -74,13 +76,9 @@ def nv_down_seq():
                     soup = BeautifulSoup(page.content, "html.parser")
 
                     for tag in soup.select('li'):
-                        tagImg = tag.find(class_='tit_wrap') # lnk_tit
-                        if tagImg is not None:
-                            sub_tagImg = tagImg.find(class_='lnk_tit')
-                            if sub_tagImg is None:
-                                sub_tagImg = tagImg.find(class_='txt_link')
-                            if str(sub_tagImg).find('플라이비치') > -1 and iflag:
-                                # print(idx, sub_tagImg)
+                        url_content = tag.find(class_='tit_wrap')
+                        if url_content is not None:
+                            if url_content is not None and url_content.text.find('플라이비치') > 0 and iflag:
                                 save_json = {'Category': ct, '키워드': key
                                             , '입찰가': int(key_val['입찰가'])
                                             , '평균클릭비용': int(key_val['평균클릭비용'])
