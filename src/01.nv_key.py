2bf279f44c9a884e5b292f9f799d6ab7e32c4f39
parksungchan <tjdcks80@poscoict.com>
2023-02-11 14:21:26 +0900

    update

--- src/01.nv_key.py
+++ src/01.nv_key.py
@@ -5,12 +5,14 @@ import pyperclip
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from src.common.get_utils import get_cofig_init
 
 '''
     네이버 광고 사이트에 각 키워드 별로 얼마의 금액이 책정되어 있는지 확인할 수 있다.
 '''
 config_info = {
-  "nv_ad_info": {"id" : "flybeach", "pw": "flyhub85!@"}
+  'run_path': os.path.abspath(__file__)
+  , "nv_ad_info": {"id" : "flybeach", "pw": "flyhub85!@"}
   , "nv_ad_url": {
       "pc": {
         "자사명": "https://manage.searchad.naver.com/customers/927013/adgroups/grp-m001-01-000001628491798",
@@ -88,43 +90,8 @@ config_info = {
     }
 }
 
-config = None
-
-
-class CustomClass:
-    def __init__(self, config_dict):
-        for con in config_dict:
-            if type(config_dict[con]) is dict:
-                self.__setattr__(con, CustomClass(config_dict[con]))
-            else:
-                self.__setattr__(con, config_dict[con])
-
-
-def get_cofig_init():
-    global config
-    config = CustomClass({})
-
-    # dir list
-    dirs = {}
-    if getattr(sys, 'frozen', False):
-        # .exe로 실행한 경우,.exe를 보관한 디렉토리의 full path를 취득
-        dirs['cur_dir'] = os.path.split(os.path.dirname(os.path.abspath(sys.executable)))[0]
-    else:
-        dirs['cur_dir'] = os.path.split(os.path.realpath(__file__))[0]
-    dirs['src_dir'] = os.path.dirname(dirs['cur_dir'])
-    dirs['pjt_dir'] = os.path.dirname(dirs['src_dir'])
-    dirs['config_dir'] = os.path.join(dirs['pjt_dir'], 'config')
-    dirs['data_dir'] = os.path.join(dirs['pjt_dir'], 'data')
-    dirs['master_dir'] = os.path.join(dirs['pjt_dir'], 'master')
-    dirs['chromedriver_dir'] = os.path.join(dirs['master_dir'], 'chromedriver')
-
-    os.makedirs(dirs['data_dir'], exist_ok=True)
-
-    config = CustomClass(config_info)
-
-    config.__setattr__('dirs', CustomClass({}))
-    for ds in dirs:
-        config.dirs.__setattr__(ds, dirs[ds])
+get_cofig_init(config_info)
+from src.common.get_utils import config
 
 
 def nv_down_excel():
@@ -138,21 +105,14 @@ def nv_down_excel():
         config.json에서 nv_ad_info, nv_ad_url 가 작성되어 있어야 한다.
 
     '''
-    get_cofig_init()
-    # 기존 다운 받으려는 키워드 파일 삭제
-    download_dir = os.path.join('C:Users', getpass.getuser(), 'Downloads')
     print('[Complete] Download Folder keyword list delete.')
-
-    # # 키워드 분석한 결과 money 디렉토리 삭제
-    # with open("E:\\0105.python\\nMonitering\\src\\common\\foo.txt", "w") as f:
-    #     f.write(config.dirs.data_dir)
-
+    # 기존 다운 받으려는 키워드 파일 삭제
     for dr in os.listdir(config.dirs.data_dir):
-        if dr.find('money_nv') > -1:
+        if dr.find(os.path.basename(config.dirs.data_sub_dir)) > -1:
             d = os.path.join(config.dirs.data_dir, dr)
             if os.path.isdir(d):
                 shutil.rmtree(d)
-    print('[Complete] Money Folder delete.')
+    print('[Complete] Key Folder delete.')
 
     # Load Chrome Driver
     driver_dir = config.dirs.chromedriver_dir
@@ -176,13 +136,13 @@ def nv_down_excel():
     driver.find_element(By.XPATH, '//*[@id="container"]/div/div/fieldset/div/div/button').click()
 
     # 광고 시스템 클릭
-    driver.find_element(By.XPATH,
-                        '//*[@id="container"]/my-screen/div/div[1]/div/my-screen-board/div/div[1]/ul/li[1]/a').click()
+    driver.find_element(By.XPATH, '//*[@id="container"]/my-screen/div/div[1]/div/my-screen-board/div/div[1]/ul/li[1]/a').click()
 
     # 상세 광고로 이동 하여 pc, mobile excel 을 구분 해서 저장해 주어야 한다
     now = datetime.datetime.now()
     now7 = (now - datetime.timedelta(days=7)).strftime("%Y.%m.%d")
     now1 = (now - datetime.timedelta(days=1)).strftime("%Y.%m.%d")
+    download_dir = os.path.join('C:Users', getpass.getuser(), 'Downloads')
     for pc_mb in config.nv_ad_url.__dict__:
         print('[Connect Start] ' + pc_mb)
         for link in config.nv_ad_url.__dict__[pc_mb].__dict__:
@@ -196,17 +156,16 @@ def nv_down_excel():
             time.sleep(2)
 
             # excel download button 클릭
-            driver.find_element(By.XPATH,
-                                '//*[@id="root"]/div/div[2]/div/div[1]/div[4]/div/div[1]/div/div/div[1]/div[1]/div[2]/div/button').click()
+            driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[1]/div[4]/div/div[1]/div/div/div[1]/div[1]/div[2]/div/button').click()
             time.sleep(2)
 
             # download에 있는 파일을 data 폴더의 money로 옮겨준다.
-            money_dir = os.path.join(config.dirs.data_dir, 'money_nv', 'money_nv_' + pc_mb)
-            os.makedirs(money_dir, exist_ok=True)
+            key_dir = os.path.join(config.dirs.data_sub_dir, pc_mb)
+            os.makedirs(key_dir, exist_ok=True)
             move_list = [x for x in os.listdir(download_dir) if x.find('키워드') > -1]
             for move in move_list:
                 src_path = os.path.join(download_dir, move)
-                trg_path = os.path.join(money_dir, link + '$' + now7 + '$' + now1 + '.xlsx')
+                trg_path = os.path.join(key_dir, link + '$' + now7 + '$' + now1 + '.xlsx')
                 shutil.move(src_path, trg_path)
 
         print('[Connect Complete] ' + pc_mb)
@@ -269,10 +228,10 @@ def nv_load_excel():
 
 '''
     1. src/make_ad_money 폴더에서 아래 명령어를 실행한다.
-    pyinstaller -w -F make_nv_excel.py # 실행 파일 하나만 만들기
+    pyinstaller -w -F 01.nv_key.py # 실행 파일 하나만 만들기
 '''
 print('[Start] ', datetime.datetime.now())
 nv_down_excel()
 time.sleep(2)
-nv_load_excel()
+# nv_load_excel()
 print('[Complete] ', datetime.datetime.now())
